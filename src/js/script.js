872203aa3ceafc45d044e6cba13b8073ecd8d5e2
Vitalii Hrytsenko <vitalik.gricenko77@gmail.com>
2023-09-27 01:07:11 +0300

    add mobile version

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,8 @@
+
+
+
+// hover cuursor
+
 let casesItems = document.querySelectorAll('.cases-item');
 
 function hoverCasesItem(e, item) {
@@ -12,3 +17,24 @@ casesItems.forEach((item) => {
   item.addEventListener('mouseover', (e) => hoverCasesItem(e, item))
   item.addEventListener('mouseout', (e) => hoverCasesItem(e, item))
 })
+
+
+// Scroll click
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  if(anchor.getAttribute('href')[0] !== '#') {
+    continue;
+  }
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
